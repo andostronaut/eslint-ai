89b0ece1e251c8ad6e3cf4cb4286a1c509bd9795
Ando RAMANAMIHANTA <iamando@tuta.io>
2023-04-20 10:22:34 +0300

    feature: prepare constants util

---
+++ src/utils/constants.ts
@@ -0,0 +1,9 @@
+import pkg from '../../package.json'
+
+export const COMMAND_NAME = pkg.name
+export const REPO_URL = pkg.repository.url
+export const VERSION = pkg.version
+export const AUTHOR = pkg.author.name
+export const PROJECT_NAME = pkg.name
+
+export const CANCELED_OP_MSG = 'Operation cancelled'
