3b58376b5228abcfb0b273d3e64d95a0fbb4e772
Ando RAMANAMIHANTA <iamando@tuta.io>
2023-04-23 20:54:08 +0300

    feature: setup log util

---
+++ src/utils/log.ts
@@ -0,0 +1,14 @@
+import { blue, green, red, dim } from 'kolorist'
+
+export const log = ({ type, msg }: { type: string; msg: string }) => {
+  switch (type) {
+    case 'info':
+      return console.info(`\n${blue('❔')} ${msg}`)
+    case 'success':
+      return console.log(`\n${green('✔')} ${msg}`)
+    case 'error':
+      return console.error(`\n${red('❌')} ${msg}`)
+    default:
+      return console.log(`\n${dim('❕')} ${msg}`)
+  }
+}
