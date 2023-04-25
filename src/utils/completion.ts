cd147b6cb688f72c235bfc80585004c6c928e9b6
Ando RAMANAMIHANTA <iamando@tuta.io>
2023-04-25 16:59:26 +0300

    feature: use completion for gpt util

--- src/utils/completion.ts
+++ src/utils/completion.ts
@@ -1,41 +1,38 @@
 import { OpenAIApi, Configuration, ChatCompletionRequestMessage } from 'openai'
-import { IncomingMessage } from 'http'
-import type { AxiosError } from 'axios'
 import dedent from 'dedent'
-
+import { IncomingMessage } from 'http'
 import { CliError } from './cli-error'
+import type { AxiosError } from 'axios'
 import { streamToString } from './stream-to-string'
 
-const getOpenAi = (key: string, apiEndpoint: string) => {
+function getOpenAi(key: string, apiEndpoint: string) {
   const openAi = new OpenAIApi(
     new Configuration({ apiKey: key, basePath: apiEndpoint })
   )
-
   return openAi
 }
 
-export const suggest = async ({
-  error,
+export const generateCompletion = async ({
+  prompt,
+  number = 1,
   key,
   model,
   apiEndpoint,
-  number = 1,
 }: {
-  error: string | ChatCompletionRequestMessage[]
-  key: string
-  model?: string
-  apiEndpoint: string
+  prompt: string | ChatCompletionRequestMessage[]
   number?: number
+  model?: string
+  key: string
+  apiEndpoint: string
 }) => {
   const openAi = getOpenAi(key, apiEndpoint)
-
   try {
     const completion = await openAi.createChatCompletion(
       {
         model: model || 'gpt-3.5-turbo',
-        messages: Array.isArray(error)
-          ? error
-          : [{ role: 'user', content: error }],
+        messages: Array.isArray(prompt)
+          ? prompt
+          : [{ role: 'user', content: prompt }],
         n: Math.min(number, 10),
         stream: true,
       },
@@ -54,7 +51,6 @@ export const suggest = async ({
 
     const response = error.response
     let message = response?.data as string | object | IncomingMessage
-
     if (response && message instanceof IncomingMessage) {
       message = await streamToString(
         response.data as unknown as IncomingMessage
@@ -67,12 +63,13 @@ export const suggest = async ({
     }
 
     const messageString = message && JSON.stringify(message, null, 2)
-
     if (response?.status === 429) {
       throw new CliError(
         dedent`
         Request to OpenAI failed with status 429. This is due to incorrect billing setup or excessive quota usage. Please follow this guide to fix it: https://help.openai.com/en/articles/6891831-error-code-429-you-exceeded-your-current-quota-please-check-your-plan-and-billing-details
+
         You can activate billing here: https://platform.openai.com/account/billing/overview . Make sure to add a payment method if not under an active grant from OpenAI.
+
         Full message from OpenAI:
       ` +
           '\n\n' +
