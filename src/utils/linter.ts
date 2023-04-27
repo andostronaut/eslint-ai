59d6d6bfefd7e9aff974313736357cfc697db9aa
Ando RAMANAMIHANTA <iamando@tuta.io>
2023-04-27 21:42:39 +0300

    feature: set empty array if empty input and use prompt intro

--- src/utils/linter.ts
+++ src/utils/linter.ts
@@ -1,6 +1,7 @@
 import { exec } from 'node:child_process'
 import path from 'path'
 import * as p from '@clack/prompts'
+import _ from 'lodash'
 
 import { CliError } from './cli-error'
 import log from './log'
@@ -9,24 +10,30 @@ import { generateCompletion } from './completion'
 import { getConfig } from './config'
 
 export const linter = async ({ input }: { input: string }) => {
-  const files = input.split(' ')
+  const files = _.isEmpty(input) ? [] : input.split(' ')
   const spin = p.spinner()
+  let cmd = ''
 
-  const hasValidExt = files.every(
-    file => path.extname(file) === '.js' || path.extname(file) === '.ts'
-  )
-
-  if (!hasValidExt) {
-    throw new CliError(
-      'An error occured, please enter a valid file with `.js or .ts` extension.'
+  if (Array.isArray(files) && files.length !== 0) {
+    const hasValidExt = files.every(
+      file => path.extname(file) === '.js' || path.extname(file) === '.ts'
     )
+
+    if (!hasValidExt) {
+      throw new CliError(
+        'An error occured, please enter a valid file with `.js or .ts` extension.'
+      )
+    }
+
+    cmd = `eslint ${
+      Array.isArray(files) && files.length !== 0 ? files.join(' ') : ''
+    }`
+  } else {
+    cmd = 'eslint .'
   }
 
-  const cmd = `eslint ${
-    Array.isArray(files) && files.length !== 0 ? files.join(' ') : ''
-  }`
-
   ;(() => {
+    p.intro('ESLint: AI')
     spin.start('Linting your code')
 
     exec(cmd, err => {
@@ -103,8 +110,11 @@ export const linter = async ({ input }: { input: string }) => {
             type: 'success',
             msg: 'All files linted successfully with no error.',
             isConsole: false,
+            newLine: false,
           }) as string
         )
+
+        p.outro('Goodbye!')
       }
     })
   })()
